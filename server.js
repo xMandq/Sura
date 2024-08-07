512101f7e586ce315876a30afc3c0c5fd9dc66af
Mandq <165655071+xMandq@users.noreply.github.com>
2024-08-07 14:35:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.post('/log-action', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
